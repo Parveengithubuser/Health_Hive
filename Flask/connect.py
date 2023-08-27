0e45cdedd179ee5b569c3f88f4d9422ab77e332a
Parveengithubuser <114249958+Parveengithubuser@users.noreply.github.com>
2023-08-27 13:55:36 +0530

    Update connect.py

--- Flask/connect.py
+++ Flask/connect.py
@@ -1,49 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 15 13:58:50 2023
+from flask import Flask, render_template, request,session
 
-@author: Shivani_SB
-"""
+import ibm_db
+
 def showall():
-    sql= "SELECT * from Contact info"
+    sql = "SELECT * from Contact info"
     stmt = ibm_db.exec_immediate(conn, sql)
     dictionary = ibm_db.fetch_both(stmt)
     while dictionary != False:
-        print("The Name is : ",  dictionary["NAME"])
-        print("The Contact is : ", dictionary["MOBILE NUMBER"])
-        print("The Email Id is : ",  dictionary["EMAIL ID"])
-        print("The Specialist is : ",  dictionary["SPECIALIST"])
-        print("The Problem is : ",  dictionary["PROBLEM"])
+        print("The name is : ",  dictionary["name"])
+        print("The mobile is : ", dictionary["mobile number"])
+        print("The email is : ",  dictionary["email id"])
+        print("The specialist is : ",  dictionary["specialist"])
+        print("The Problem is : ",  dictionary[" prolem"])
         dictionary = ibm_db.fetch_both(stmt)
-        
-def getdetails(name,specialization):
-    sql= "select * from Contact info where name='{}' and specialization='{}'".format(name,specialization)
+
+def insert_db(conn,name,mobile,email,specialist,problem):
+    sql = "INSERT into APPOINTMENT VALUES('{}', '{}', '{}', '{}', '{}')".format(name,mobile,email,specialist,problem)
     stmt = ibm_db.exec_immediate(conn, sql)
-    dictionary = ibm_db.fetch_both(stmt)
-    while dictionary != False:
-        print("The Name is : ",  dictionary["NAME"])
-        print("The Contact is : ", dictionary["MOBILE NUMBER"])
-        print("The Email Id is : ",  dictionary["EMAIL ID"])
-        print("The Specialist is : ",  dictionary["SPECIALIST"])
-        print("The Problem is : ",  dictionary["PROBLEM"])
-        dictionary = ibm_db.fetch_both(stmt)
-        
-def insertdb(conn,name,contact,email,specialist,problem):
-    sql= "INSERT into Contact info VALUES('{}','{}','{}','{}','{}')".format(name,contact,email,specialist,problem)
-    stmt = ibm_db.exec_immediate(conn, sql)
-    print ("Number of affected rows: ", ibm_db.num_rows(stmt))
+    print("Number of affected rows: ", ibm_db.num_rows(stmt))
+
+    
 
 try:
-    import ibm_db
     conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30699;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=bcw97627;PWD=uA6r9OTuX4lZUOCC",'','')
     print(conn)
-    print("connection successful...")
-    #insertdb(conn,"Dr.K.Srinivas Reddy",)
-    getdetails("Dr.K.Srinivas Reddy","Cardiologist")
-    #showall()
+    print("Connection successful...")
+    
+    # Call the function to show all records
+    showall()
+ # Example of inserting a new record
+    #insert_db(conn, "Cardiology", "Dr. Smith", "2023-08-26", "10:00 AM", "John Doe", 1234567890, "Heart issues")
+    
+   
 
-except:
-    print("Error connecting to the database")
-
-
-
+except Exception as e:
+    print("Error connecting to the database:", e)
